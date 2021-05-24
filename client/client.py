7b3307eb2ee6328b8d82ebf811afdccfebaf0298
Tran Duy Tien <54745802+duytienkh@users.noreply.github.com>
2021-05-25 00:42:46 +0700

    update

--- client/client.py
+++ client/client.py
@@ -1,14 +1,5 @@
-import socket
-
-
-def connect_server(server_address):
-    print("Try to connect server", server_address)
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn.connect(server_address)
-
-    return conn
+import modules.login as login
 
 
 if __name__ == "__main__":
-    conn = connect_server(("127.0.0.1", 55555))
-    conn.close()
+    login.login_gui()
