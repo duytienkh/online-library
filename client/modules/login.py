224c450d9592fb546a72e573f02c7147024cd327
Tran Duy Tien <54745802+duytienkh@users.noreply.github.com>
2021-05-25 00:42:30 +0700

    sign up and sign in features

--- client/modules/login.py
+++ client/modules/login.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+import modules.commute as commute
+
 
 def login_gui():
     gui = tk.Tk(className="login")
@@ -21,7 +23,27 @@ def login_gui():
     btn_frame = tk.Frame(gui)
     sup_btn = tk.Button(btn_frame, text="sign up")
     sup_btn.pack(side=tk.LEFT, padx=10)
+    sup_btn.config(command=lambda: sign_up(u, p))
     sin_btn = tk.Button(btn_frame, text="sign in")
     sin_btn.pack(side=tk.RIGHT, padx=10)
+    sin_btn.config(command=lambda: sign_in(u, p))
     btn_frame.pack()
     gui.mainloop()
+
+
+def sign_in(u, p):
+    package = {
+        "type": "sign_in",
+        "username": u.get(),
+        "password": p.get()
+    }
+    commute.send(package)
+
+
+def sign_up(u, p):
+    package = {
+        "type": "sign_up",
+        "username": u.get(),
+        "password": p.get
+    }
+    commute.send(package)
