7ba5fd5392ed6bc8fcd01286b2bd990d7f0a2c59
Tran Duy Tien <54745802+duytienkh@users.noreply.github.com>
2021-05-25 21:34:03 +0700

    Create connection on first use

--- client/modules/commute.py
+++ client/modules/commute.py
@@ -1,15 +1,25 @@
 import json
 import socket
 import sys
+import xml.dom.minidom as minidom
 
 conn = None
+
+
+def create_connection():
+    if not conn:
+        doc = minidom.parse("config.xml")
+        addr = doc.getElementsByTagName("address")[0].attributes["value"].value
+        port = doc.getElementsByTagName("port")[0].attributes["value"].value
+        connect(addr, int(port))
 
 
 def connect(addr, port=55555):
     global conn
     try:
+        print("Try to connect...")
         sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sk.connect(addr, port)
+        sk.connect((addr, port))
         conn = sk
     except ConnectionRefusedError:
         print("show error msg")
@@ -17,6 +27,8 @@ def connect(addr, port=55555):
 
 
 def send(package):
+    if not conn:
+        create_connection()
     print(package)
     package = json.dumps(package).encode()
     req_size = {"size": sys.getsizeof(package)}
