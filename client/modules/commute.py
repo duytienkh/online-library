a36ababb13dcf0659fb41aa7086e2c3e8e4c232a
Tran Duy Tien <54745802+duytienkh@users.noreply.github.com>
2021-05-25 14:47:54 +0700

    print package

--- client/modules/commute.py
+++ client/modules/commute.py
@@ -16,8 +16,11 @@ def connect(addr, port):
 
 
 def send(package):
-    req_size = json.dumps({"size": package.size()}).encode()
+    req_size = {"size": package.size()}
+    print(req_size)
+    req_size = json.dumps(req_size).encode()
     conn.sendall(req_size)  # send req size
+    print(package)
     req = json.dumps(package).encode()
     conn.sendall(req)  # send req
 
@@ -25,5 +28,8 @@ def send(package):
 def recv():
     res_size = conn.recv(128)  # recv res size
     res_size = json.loads(res_size.decode())
-    res = conn.recv(res_size["size"]).decode()  # recv res
+    print(res_size)
+    res = conn.recv(res_size["size"])  # recv res
+    res = json.loads(res.decode())
+    print(res)
     return res
